a403485829a70dc19404d8a5aa821f5ae9094624
Kartik Tyagi <kartiktyagi11@gmail.com>
2024-09-27 18:53:34 -0400

    did 1.3

--- hello.py
+++ hello.py
@@ -8,7 +8,3 @@ def index():
 @app.route('/user/<name>')
 def user(name):
   return '<h1>Hello, {}!</h1>'.format(name)
-
-@app.route('/index')
-def runIndex():
-  return render_template('index.html')
