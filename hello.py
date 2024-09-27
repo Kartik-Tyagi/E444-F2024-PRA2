8664b0c417e52f998bf0244a78e31d857699c3da
Kartik Tyagi <kartiktyagi11@gmail.com>
2024-09-27 17:59:34 -0400

    added html file

--- hello.py
+++ hello.py
@@ -8,3 +8,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
   return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/index')
+def runIndex():
+  return render_template('index.html')
