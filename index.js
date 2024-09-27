05c0db49e6ef2597a15d775088b00eeb76629a24
Kartik Tyagi <kartiktyagi11@gmail.com>
2024-09-27 18:26:10 -0400

    added css and js files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let currentDate = new Date()
+
+let year = currentDate.getFullYear();
+let month = currentDate.getMonth() + 1;
+let day = currentDate.getDate();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+
+//let dateTime = Document
